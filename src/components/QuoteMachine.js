f0fdedeae5b96d368aee3722d4cb98f5a501d04a
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2020-09-19 07:53:30 +0700

    commit

---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,36 @@
+import React from 'react';
+//material-ui
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+//fontAwesome 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
+
+const QuoteMachine = (props) => (
+    <Card>
+        <CardContent>
+            <Typography id="text">
+                {props.selectedQuote.quote} - <span id="author">{props.selectedQuote.author}</span>
+            </Typography>
+        </CardContent>
+        
+        <CardActions>
+            <IconButton 
+                id="tweet-quote"
+                target="_blank"
+                href={`https://twitter.com/intent/tweet?text=${props.selectedQuote.quote}&hashtags=motivated-quote`}
+            >
+                <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+            </IconButton>
+            <Button id="new-quote" size="medium" onClick={props.assignNewQuoteIndex}>Next Quote</Button>
+        </CardActions>
+    </Card>
+);
+
+export default QuoteMachine;
